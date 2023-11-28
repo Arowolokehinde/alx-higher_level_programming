a9922feb42935f50a5f72342e4691f9854284fba
Arowolokehinde <arowolokehinde321@gmail.com>
2023-11-28 12:40:43 -0800

    11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    result = 1
+
+    # Handle the case when b is negative
+    if b < 0:
+        a = 1 / a
+        b = abs(b)
+
+    for _ in range(b):
+        result *= a
+
+    return result
