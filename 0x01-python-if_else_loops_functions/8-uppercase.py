b44f0832990929ce2cd8502ed16ce54c508393a2
Arowolokehinde <arowolokehinde321@gmail.com>
2023-11-28 12:01:20 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-def uppercase(s):
-    for char in s:
-        # Convert the character to uppercase using ASCII values
-        upper_char = chr(ord(char) - ord('a') + ord('A')) \
-            if 'a' <= char <= 'z' else char
-        print(upper_char, end='')
-    print()
+
+def uppercase(str):
+    for char in str:
+        if 'a' <= char <= 'z':
+            print(chr(ord(char) - 32), end='')
+
+
+print()
