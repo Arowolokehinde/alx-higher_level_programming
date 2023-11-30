bfa07e5f5ef654ff039dd42e027cbb92b1cb4025
Arowolokehinde <arowolokehinde321@gmail.com>
2023-11-30 04:56:48 -0800

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    result_1 = add(a, b)
+    result_2 = sub(a, b)
+    result_3 = mul(a, b)
+    result_4 = div(a, b)
+
+    print("{} + {} = {}".format(a, b, result_1))
+    print("{} - {} = {}".format(a, b, result_2))
+    print("{} * {} = {}".format(a, b, result_3))
+    print("{} / {} = {}".format(a, b, result_4))
