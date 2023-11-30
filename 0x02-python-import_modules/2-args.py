872bac004606d044bfbf2969c09c51692c4d5931
Arowolokehinde <arowolokehinde321@gmail.com>
2023-11-30 09:31:41 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    num = len(argv) - 1
-    if num == 0:
-        print("{} arguments.".format(num))
-    elif num == 1:
-        print("{} arguments:".format(num))
-        print("1:{}".format(argv[1]))
+    n = len(argv) - 1
+    if n == 0:
+        print("{} arguments.".format(n))
+    elif n == 1:
+        print("{} argument:".format(n))
+        print("1: {}".format(argv[1]))
     else:
-        print("{} arguments:".format(num))
+        print("{} arguments:".format(n))
         i = 1
         for arg in argv[1:]:
             print("{}: {}".format(i, arg))
