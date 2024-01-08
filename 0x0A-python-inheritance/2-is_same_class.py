8abecac201b70f642cbab657c2ceb0e224f3a07e
Arowolokehinde <arowolokehinde321@gmail.com>
2024-01-08 08:10:46 -0800

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that checks for instance"""
+
+
+def is_same_class(obj, a_class):
+    """returns true if it is an instance of a_class"""
+    if type(obj) is a_class:
+        return True
