210d5f270eb11bc1e0a92e2f74bb736f8edd82ec
Arowolokehinde <arowolokehinde321@gmail.com>
2024-01-08 08:33:40 -0800

    4-main.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+ function that returns True if the object is an instance 
+"""
+def inherits_from(obj, a_class):
+    """ returns true else false if otherwise"""
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
