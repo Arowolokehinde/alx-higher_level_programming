bbcc7cbc0cb06b430b5499af4acf1e4b9c8036ae
Arowolokehinde <arowolokehinde321@gmail.com>
2024-01-08 08:25:14 -0800

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Function that check for instance of a class and subclass
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True if found else false"""
+    return isinstance(obj, a_class)
