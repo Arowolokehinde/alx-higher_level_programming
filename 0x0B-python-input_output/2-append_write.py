c2c9b9900ae7b7f4f81dd9be6361a504d765d7ee
Arowolokehinde <arowolokehinde321@gmail.com>
2024-01-09 10:51:31 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,4 +7,4 @@ contains the append file
 def append_write(filename="", text=""):
     """returns the number of characters appended to the string"""
     with open(filename, "a", encoding="utf-8") as f:
-        return(f.write(text))
+        return f.write(text)
