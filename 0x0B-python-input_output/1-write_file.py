8a8bfa8fc7e88615538e9ae04a647b223d32f9fa
Arowolokehinde <arowolokehinde321@gmail.com>
2024-01-09 08:49:50 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+contains the write file
+"""
+
+def write_file(filename="", text=""):
+    """function that writes a string to a text file and returns the number """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
