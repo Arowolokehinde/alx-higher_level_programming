bc3aced1c9184a3caa8e3f51719c9493f241cd42
Arowolokehinde <arowolokehinde321@gmail.com>
2024-01-09 10:48:51 -0800

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+contains the append file
+"""
+
+
+def append_write(filename="", text=""):
+    """returns the number of characters appended to the string"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return(f.write(text))
