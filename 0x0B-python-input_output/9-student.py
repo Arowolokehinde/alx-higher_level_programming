457cfee8e21503ceaf030904a742b8b916b09490
Arowolokehinde <arowolokehinde321@gmail.com>
2024-01-11 06:10:33 -0800

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that defines the class student
+"""
+
+class Student:
+    """class to create student class """
+    def __init__(self, first_name, last_name, age):
+        """ instantiation"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieves a dictionary representration"""
+        return self.__dict__.copy()
