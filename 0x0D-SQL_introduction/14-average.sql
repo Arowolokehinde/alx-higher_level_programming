ad690b0b7961e0452513c2b1073c155b9ecd0855
Arowolokehinde <arowolokehinde321@gmail.com>
2024-02-14 06:53:34 -0800

    a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- a script that computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
 -- the table name is second_table
-SELECT AVG(score) as Average
+SELECT AVG(score) AS Average
 FROM second_table;
