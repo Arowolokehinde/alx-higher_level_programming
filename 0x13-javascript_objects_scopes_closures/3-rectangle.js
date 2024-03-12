32508dc18232068cda481c82c13778096c0a4be3
arowolokehinde <arowolokehinde321@gmail.com>
2024-03-12 13:59:59 -0400

    a class Rectangle that defines a rectangle: with 'X'

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
 #!/bin/usr/node
 
 //  a class Rectangle that defines a rectangle:
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -19,5 +19,3 @@ class Rectangle {
     }
   }
 }
-
-module.exports = Rectangle;
