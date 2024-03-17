e3460e4297c9df59df36b28ed269b7278326c8d8
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-03-17 15:58:27 -0400

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 
+"""  a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
 
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1], password=sys.argv[2], database=sys.argv[3], port=3306)
+
+    curso = database.cursor()
+    curso.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
+
+    states = curso.fetchall()
+    for state in states:
+        print(state)
+
+    curso.close()
+    database.close()
