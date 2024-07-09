821db9a7b18eab013debe01547523db60fbd66cd
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-07-09 15:30:03 +0100

    [200~a script that prints the addition of 2 integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* eslint-disable quotes */
+
+const { argv } = require("node:process");
+
+function add(a, b) {
+  return a + b;
+}
+
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
+
+if (isNaN(a) || isNaN(b)) {
+  console.log(`NaN`);
+} else {
+  console.log(add(a, b));
+}
